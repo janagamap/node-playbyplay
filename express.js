695c0937e50177167ca299f134764b573778dda4
Prashanth Janagama <prashanth2980@gmail.com>
2016-08-31 06:18:37 -0400

    cats-crud

--- express.js
+++ express.js
@@ -1,10 +1,21 @@
 var express = require('express');
 var app = express();
 
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+	extended: true;
+}));
+
+/*
 app.get('/', function(req, res){
 	//res.send('Hello World!');
-	res.json({hello:'world'});
+	res.json({hello  :'world'});
 });
+*/
+
+var cats = require('./cats.js')(app);
 
 var server = app.listen(3000, function(){
 	console.log('Server running at htpp://127.0.0.1:3000/');
